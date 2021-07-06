d37868942bd5c9d39763aaa91ebbd426a17465b1
apal <alishapal96@gmail.com>
2021-07-05 23:35:30 -0700

    add fetch request

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("linked")
+var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=2ac30b0842ba2d3340555a86403c9a45";
+var submitButton = document.querySelector(".fa-search");
+var cityName = $(".citySearched").val();
+
+console.log(cityname)
+console.log(submitButton)
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+function getApi(){
+    cityUrl = requestUrl + 
+    fetch(citytUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+    };
+function displayWeather(event){
+    event.preventDefault();
+    console.log("button works");
+}
+
+submitButton.addEventListener('click', displayWeather);
